4307cb4c87615ad40eee87928a1185d61d18752a
Rohan Jayasekera <rjayasekera@fec.gov>
2019-04-01 12:25:58 -0400

    Added back transition zone

--- terraform/test.fec.gov.tf
+++ terraform/test.fec.gov.tf
@@ -8,12 +8,12 @@
 # and because you can't CNAME the root domain, Savvis/CenturyLink are 
 # pointing the root at an FEC server that HTTP redirects to www.fec.gov
 
-#resource "aws_route53_zone" "transition_gov_us_zone" {
-#  name = "transition.fec.gov"
-#  tags {
-#    Project = "dns"
-#  }
-#}
+resource "aws_route53_zone" "transition_gov_us_zone" {
+ name = "transition.fec.gov"
+ tags {
+   Project = "dns"
+ }
+}
 
 resource "aws_route53_zone" "www_fec_gov_zone" {
   name = "www.fec.gov"
@@ -40,16 +40,16 @@ resource "aws_route53_record" "stage_gov_stage_gov_cname" {
   }
 }
 
-#resource "aws_route53_record" "transition_gov_transition_gov_cname" {
-#  zone_id = "${aws_route53_zone.transition_gov_us_zone.zone_id}"
-#  name = "transition.fec.gov"
-#  type = "A"
-#  alias {
-#    name = "d2p6ccc3xlipxg.cloudfront.net"
-#    zone_id = "${local.cloudfront_zone_id}"
-#    evaluate_target_health = false
-#  }
-#}
+resource "aws_route53_record" "transition_gov_transition_gov_cname" {
+ zone_id = "${aws_route53_zone.transition_gov_us_zone.zone_id}"
+ name = "transition.fec.gov"
+ type = "A"
+ alias {
+   name = "d2p6ccc3xlipxg.cloudfront.net"
+   zone_id = "${local.cloudfront_zone_id}"
+   evaluate_target_health = false
+ }
+}
 
 resource "aws_route53_record" "www_fec_gov_a_alias" {
   zone_id = "${aws_route53_zone.www_fec_gov_zone.zone_id}"
@@ -62,9 +62,9 @@ resource "aws_route53_record" "www_fec_gov_a_alias" {
   }
 }
 
-#output "transition_gov_us_ns" {
-#  value="${aws_route53_zone.transition_gov_us_zone.name_servers}"
-#}
+output "transition_gov_us_ns" {
+ value="${aws_route53_zone.transition_gov_us_zone.name_servers}"
+}
 
 output "www_fec_gov_ns" {
   value="${aws_route53_zone.www_fec_gov_zone.name_servers}"
