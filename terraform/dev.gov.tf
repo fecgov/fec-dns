94dbede92a164239c819ef66cba83abff299722f
Rohan Jayasekera <rjayasekera@fec.gov>
2019-05-12 11:46:03 -0400

    Add feccom-dev.fec.gov Alias to Cloudfront

--- terraform/dev.gov.tf
+++ terraform/dev.gov.tf
@@ -3,6 +3,7 @@
 # Add A record pointing to Cloudfront (value from cloud.gov fec-dev-cdn-route service)
 # Add TXT record (if needed) pointing to _acme-challenge (value from cloud.gov fec-dev-cdn-route service)
 
+
 resource "aws_route53_zone" "dev_gov_zone" {
   name = "dev.fec.gov"
   tags {
@@ -11,16 +12,16 @@ resource "aws_route53_zone" "dev_gov_zone" {
 }
 
 
-#resource "aws_route53_record" "dev_gov_a_alias" {
-#  zone_id = "${aws_route53_zone.dev_gov_zone.zone_id}"
-#  name = "dev.fec.gov"
-#  type = "A"
-#  alias {
-#    name = "d18qvbems6nrkf.cloudfront.net"
-#    zone_id = "Z2FDTNDATAQYW2"
-#    evaluate_target_health = false
-#  }
-#}
+resource "aws_route53_record" "dev_gov_a_alias" {
+  zone_id = "${aws_route53_zone.dev_gov_zone.zone_id}"
+  name = "dev.fec.gov"
+  type = "A"
+  alias {
+    name = "d18qvbems6nrkf.cloudfront.net"
+    zone_id = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
 
 
 output "dev_gov_ns" {
