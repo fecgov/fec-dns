5e7239ab6e814a3df1ab813df331747d50125838
Rohan Jayasekera <rjayasekera@fec.gov>
2019-05-21 20:32:47 -0400

    Add Alias record set for www.fec.gov

--- terraform/www.gov.tf
+++ terraform/www.gov.tf
@@ -12,16 +12,16 @@ resource "aws_route53_zone" "www_gov_zone" {
 }
 
 
-#resource "aws_route53_record" "www_gov_a_alias" {
-#  zone_id = "${aws_route53_zone.www_gov_zone.zone_id}"
-#  name = "www.fec.gov"
-#  type = "A"
-#  alias {
-#    name = "d3t5a36r5g3qx4.cloudfront.net"
-#    zone_id = "Z2FDTNDATAQYW2"
-#    evaluate_target_health = false
-#  }
-#}
+resource "aws_route53_record" "www_gov_a_alias" {
+  zone_id = "${aws_route53_zone.www_gov_zone.zone_id}"
+  name = "www.fec.gov"
+  type = "A"
+  alias {
+    name = "d3t5a36r5g3qx4.cloudfront.net"
+    zone_id = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
 
 
 output "www_gov_ns" {
